74c9f1f7aa6ef8c20dc2103244c2ef75c29cccab
Snivas1 <nivasn227@gmail.com>
2026-06-19 22:48:31 +0530

    Added dynamic profile data to dashboard

--- lib/models/user_profile.dart
+++ lib/models/user_profile.dart
@@ -2,11 +2,15 @@ class UserProfile {
   static String name = "";
   static String email = "";
   static String password = "";
+
   static String phone = "";
   static String college = "";
   static String branch = "";
   static String year = "";
   static String careerGoal = "";
 
+  static List<String> selectedSkills = [];
   static int skillsCount = 0;
+
+  static String recommendedCareer = "";
 }
